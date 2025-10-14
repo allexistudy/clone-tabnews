972e5ba09d88522ed17513b3da83419fe36a4f2c
Allexis <allexisssoares@gmail.com>
2025-10-14 14:40:19 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
-  expect.assertions(2);
   const response = await fetch("http://localhost:3000/api/v1/status");
-  const json = await response.json();
   expect(response.status).toBe(200);
-  expect(json).toEqual({ status: "ok" });
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
