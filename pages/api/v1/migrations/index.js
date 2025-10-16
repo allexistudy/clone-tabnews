c731488bb5d22353cc2c3d5fa2cb74a5870e2722
Allexis <allexisssoares@gmail.com>
2025-10-16 09:35:28 -0300

    chore: fix `/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,34 +6,40 @@ export default async function migrations(request, response) {
   const { method } = request;
 
   if (method !== "GET" && method !== "POST") {
-    return response.status(405).end();
+    return response.status(405).json({ error: `Method ${method} not allowed` });
   }
 
-  const dbClient = await database.getNewClient();
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const migrationsRunnerOptions = {
+      dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  const migrationsRunnerOptions = {
-    dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+    if (method === "GET") {
+      const pendingMigrations = await migrationRunner(migrationsRunnerOptions);
+      return response.status(200).json(pendingMigrations);
+    }
 
-  if (method === "GET") {
-    const pendingMigrations = await migrationRunner(migrationsRunnerOptions);
+    if (method === "POST") {
+      const migrationsApplied = await migrationRunner({
+        ...migrationsRunnerOptions,
+        dryRun: false,
+      });
+
+      return response
+        .status(migrationsApplied.length > 0 ? 201 : 200)
+        .json(migrationsApplied);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
     await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (method === "POST") {
-    const migrationsApplied = await migrationRunner({
-      ...migrationsRunnerOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    return response
-      .status(migrationsApplied.length > 0 ? 201 : 200)
-      .json(migrationsApplied);
   }
 }
